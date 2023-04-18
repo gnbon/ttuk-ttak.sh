10865187217b20ef56478058cfce9375f2bb3e7f
gnbon <kiddo.pwn@gmail.com>
2023-04-18 13:53:55 +0900

    zsh: add omz plugins(sudo, autojump)

--- zsh/run.sh
+++ zsh/run.sh
@@ -10,6 +10,7 @@ chsh -s /usr/bin/zsh
 # install plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+sudo apt install -y autojump
 
 # copy zshrc
 cp zshrc ~/.zshrc
