88f392493be310f3ab8a0553f3227f604e0c2747
kiddo <kiddo.pwn@gmail.com>
2023-06-12 22:12:51 -0400

    edit country code regex

--- apt/run.sh
+++ apt/run.sh
@@ -2,7 +2,7 @@
 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo sed -i 's/\w{2}.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo sed -i 's/security.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo sed -i 's/extras.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 
